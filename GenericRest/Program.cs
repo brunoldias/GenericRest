3b551351403f30fee5b5a7068686f9ca3b7af1ed
Bruno Lourenço <brunolourencodias@hotmail.com.br>
2021-08-07 13:43:05 -0300

    Add project files.

---
+++ GenericRest/Program.cs
@@ -0,0 +1,23 @@
+﻿using GenericRest.Concrats;
+using GenericRest.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+
+namespace GenericRest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            var client = new RestClientService("https://viacep.com.br");
+
+            var response = client.ExecuteAsync<CEP>("/ws/01001000/json/", null, RestSharp.Method.GET).Result;
+
+            Console.WriteLine(JsonConvert.SerializeObject(response));
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ GenericRest/RestClientService.cs
@@ -0,0 +1,39 @@
+﻿using GenericRest.Concrats;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericRest.Domain
+{
+    public class RestClientService 
+    {
+        private readonly RestClient _client;
+        public RestClientService(string url)
+        {
+            _client = new RestClient(url);
+        }
+
+        public Task<T> ExecuteAsync<T>(string resource, string body, Method method) where T : class
+        {
+            var request = new RestRequest(resource, method);
+
+            if (!string.IsNullOrEmpty(body))
+                request.AddJsonBody(body);
+
+            var response = _client.ExecuteGetAsync(request).Result;
+
+            if (response.Content is null) throw new ArgumentNullException($"Url: {response.ResponseUri} Status Code: {response.StatusCode} Error Message: Response is null");
+
+            if (response.StatusCode != HttpStatusCode.OK) throw new Exception($"Url: {response.ResponseUri} Response Content: {response.Content} Status Code: {response.StatusCode} Error Message: Occourred an error");
+
+            var data = JsonConvert.DeserializeObject<T>(response.Content);
+
+            return Task.FromResult(data);
+        }
+
+    }
+}
